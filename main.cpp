ceb9489d474a7c267e690502a9417e41f9e67bb8
Tahmid Muntaser <115565668+TahmidMuntaser@users.noreply.github.com>
2022-12-15 00:19:31 +0600

    Update 2.0.2

--- main.cpp
+++ main.cpp
@@ -6,7 +6,7 @@ int space[3][3] = {{1, 2, 3},{4, 5, 6}, {7, 8, 9}};
     // 3 rows and 3 coloums....
 
 int row, coloum; //Global variables..
-char token = 'x';
+char token = 'X';
 
 
 void funtionStr()
@@ -45,11 +45,113 @@ void Value()
 {
     int digit;
 
-    if (token == 'x')
+    if (token == 'X')
     {
         cout<<p1<<"please enter: ";
         cin>>digit;
     }
-    
 
+     if (token == 'O')
+    {
+        cout<<p2<<"please enter: ";
+        cin>>digit;
+    }
+
+    if (digit == 1)
+    {
+        row = 0;        
+        coloum = 0;
+        // position = 1
+    }
+
+     if (digit == 2)
+    {
+        row = 0;        
+        coloum = 1;
+        // position = 2
+    }
+
+     if (digit == 3)
+    {
+        row = 0;        
+        coloum = 2;
+        // position = 3
+    }
+
+     if (digit == 4)
+    {
+        row = 1;        
+        coloum = 0;
+        // position = 4
+    }
+
+     if (digit == 5)
+    {
+        row = 1;        
+        coloum = 1;
+        // position = 5
+    }
+
+     if (digit == 6)
+    {
+        row = 1;        
+        coloum = 2;
+        // position = 6
+    }
+
+     if (digit == 7)
+    {
+        row = 2;        
+        coloum = 0;
+        // position = 7
+    }
+
+     if (digit == 8)
+    {
+        row = 2;        
+        coloum = 1;
+        // position = 8
+    }
+
+
+     if (digit == 9)
+    {
+        row = 2;        
+        coloum = 2;
+        // position = 9
+    }
+
+    else
+    {
+        cout<<"Invalid Input!"<<endl;
+        cout<<"Try Again"<<endl;
+    }
+
+
+    if (token == 'X' && space[row][coloum] != 'X' && space[row][coloum] != 'O')
+    {
+        space[row][coloum] = 'X';
+        token = 'O';
+
+    }
+
+    else if (token == 'O' && space[row][coloum] != 'X' && space[row][coloum] != 'O')
+    {
+        space[row][coloum] = 'O';
+        token = 'X';
+    }
+
+    else
+    {
+        cout<<"There is no empty space"<<endl;
+        Value();
+        //To play it again
+    }
+     
+
+}
+
+
+bool funthree(){
+    
 }
